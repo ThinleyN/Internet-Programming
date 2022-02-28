c7542521828d490dfb182b44866972f84fc47b49
ThinleyN <thinleytyrant@gmail.com>
2022-02-28 17:20:22 +0000

    add mouseenter and mouseleave events in exercise 4 week 4

--- Week 4/exercise5.js
+++ Week 4/exercise5.js
@@ -8,9 +8,21 @@ function paragraphClick() {
     h1.innerHTML = "p changed";
 }
 
+function h1Entered(){
+    const h1 = document.getElementById("heading");
+    h1.style.color = "red";
+}
+
+function h1Exit() {
+    const h1 = document.getElementById("heading");
+    h1.style.color = "black";
+}
+
 function loaded() {
     const h1 = document.getElementById("heading");
-    h1.addEventListener("click", h1Click)
+    h1.addEventListener("click", h1Click);
+    h1.addEventListener("mouseenter", h1Entered);
+    h1.addEventListener("mouseleave", h1Exit);
 
     const paragraph = document.getElementById("paragraph");
     paragraph.addEventListener("click", paragraphClick)
