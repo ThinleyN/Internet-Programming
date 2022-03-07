37f8d81ff87a56a52f498ef7abaeafd1d535332b
ThinleyN <thinleytyrant@gmail.com>
2022-03-07 17:00:32 +0000

    move button to left on document keydown

--- Week5/exercise1.js
+++ Week5/exercise1.js
@@ -1,8 +1,5 @@
 function buttonClick() {
-    // setInterval(opacityTimer,10);
-    const button = document.getElementById("circle");
-    const leftPosition = button.offsetLeft;
-    button.style.left = (leftPosition - 10) + "px";
+    setInterval(opacityTimer,10);
 }
 
 function opacityTimer() {
@@ -11,9 +8,16 @@ function opacityTimer() {
     button.style.opacity = parseFloat(button.style.opacity) - 0.01;
 }
 
+function moveButton(){
+    const button = document.getElementById("circle");
+    const leftPosition = button.offsetLeft;
+    button.style.left = (leftPosition - 10) + "px";
+}
+
 function onload() {
     const button = document.getElementById("circle");
     button.style.opacity = 1;
     button.addEventListener("click", buttonClick)
+    document.addEventListener("keydown", moveButton);
 }
 document.addEventListener("DOMContentLoaded", onload);
