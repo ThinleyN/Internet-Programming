7c2286bab447895c171b199eebdfba563dba02f1
ThinleyN <thinleytyrant@gmail.com>
2022-03-07 14:57:27 +0000

    make different function to show value

--- Week 4/exercise6.js
+++ Week 4/exercise6.js
@@ -6,9 +6,19 @@ function buttonClick() {
     valueDiv.innerHTML = value;
 }
 
+function showValue() {
+    const input = document.getElementById("text");
+    const value = input.value;
+
+    alert(value);
+}
+
 function load() {
     const text = document.getElementById("text");
-    text.addEventListener("keyup",buttonClick )
+    text.addEventListener("keyup",buttonClick );
+
+    const button = document.getElementById("button")
+    button.addEventListener("click", showValue);
 }
 
 document.addEventListener("DOMContentLoaded", load);
