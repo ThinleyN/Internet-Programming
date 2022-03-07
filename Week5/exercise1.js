52c37bf2410745fd19415b655dfc80c1f4de473f
ThinleyN <thinleytyrant@gmail.com>
2022-03-07 16:14:05 +0000

    make the button more transparent after clicking

--- Week5/exercise1.js
+++ Week5/exercise1.js
@@ -1,10 +1,11 @@
 function buttonClick() {
     const button = document.getElementById("circle");
     button.style.background = "blue";
-    button.style.opacity = 1;
+    button.style.opacity = parseFloat(button.style.opacity) - 0.1;
 }
 function onload() {
     const button = document.getElementById("circle");
+    button.style.opacity = 1;
     button.addEventListener("click", buttonClick)
 }
 document.addEventListener("DOMContentLoaded", onload);
