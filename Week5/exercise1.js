d9e4e52553205fcba9ee1036893d11f9248afa98
ThinleyN <thinleytyrant@gmail.com>
2022-03-07 16:39:31 +0000

    add setinterval to reduce opacity overtime

--- Week5/exercise1.js
+++ Week5/exercise1.js
@@ -1,8 +1,13 @@
 function buttonClick() {
+    setInterval(opacityTimer,10);
+}
+
+function opacityTimer() {
     const button = document.getElementById("circle");
     button.style.background = "blue";
-    button.style.opacity = parseFloat(button.style.opacity) - 0.1;
+    button.style.opacity = parseFloat(button.style.opacity) - 0.01;
 }
+
 function onload() {
     const button = document.getElementById("circle");
     button.style.opacity = 1;
